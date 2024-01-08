beb4fb00907b9c0157586287a72100eafd91a856
Ramona Modrogan <ramona.modrogan@gmail.com>
2024-01-08 11:49:44 +0200

    Add project files.

---
+++ UIAutomationFrameworkExercise/Helpers/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using NsTestFrameworkUI.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UIAutomationFrameworkExercise.Helpers;
+
+public class BaseTest
+{
+    [TestInitialize]
+    public void Before()
+    {
+        Browser.InitializeDriver(new DriverOptions
+        {
+            ChromeDriverPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+        });     
+        Browser.GoTo("https://automationintesting.online/#/");
+    }
+}
---
+++ UIAutomationFrameworkExercise/Tests/BookingRoomTests.cs
@@ -0,0 +1,18 @@
+﻿using NsTestFrameworkUI.Helpers;
+using UIAutomationFrameworkExercise.Helpers;
+
+namespace UIAutomationFrameworkExercise.Tests;
+
+[TestClass]
+public class BookingRoomTests : BaseTest
+{
+    [TestMethod]
+
+    public void  WhenBookingARoomSuccessMessageShouldBeDisplayedTest()
+    {
+        Console.WriteLine("hello");
+        WaitHelpers.ExplicitWait();
+    }
+
+}
+
